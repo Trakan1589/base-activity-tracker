d45d098b5c6de18e49f86a40660a41b796d25508
trakan.eth <142104229+Trakan1589@users.noreply.github.com>
2026-03-04 06:52:33 +0700

    Update script.js
    Implement wallet export to JSON file

--- script.js
+++ script.js
@@ -78,3 +78,15 @@ function clearAll() {
   saveWallets();
   renderWallets();
 }
+function exportWallets() {
+  const dataStr = JSON.stringify(wallets, null, 2);
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "wallets.json";
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
